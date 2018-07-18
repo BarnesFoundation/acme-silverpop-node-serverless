5fddab7c60aac285bf51f332c6a6cf9fe6415eb9
Christopher Jativa <cjativa@barnesfoundation.org>
2018-07-18 13:17:05 -0400

    completed PersonItem class definition

--- src/classes/personItem.ts
+++ src/classes/personItem.ts
@@ -13,4 +13,59 @@ export class PersonItem {
     private _membershipLevelName: string;
     private _membershipOfferingName: string;
 
+    /** Constructor */
+
+    constructor(email: string, contactFirstName: string, contactLastName: string, zipCode: string, membershipExternalId: string, 
+        membershipPrimaryFirstName: string, membershipPrimaryLastName: string, membershipLevelName: string, membershipOfferingName: string) {
+
+        this._email = email;
+        this._contactFirstName = contactFirstName;
+        this._contactLastName = contactLastName;
+        this._zipCode = zipCode;
+
+        this._membershipExternalId = membershipExternalId;
+        this._membershipPrimaryFirstName = membershipPrimaryFirstName;
+        this._membershipPrimaryLastName = membershipPrimaryLastName;
+        this._membershipLevelName = membershipLevelName;
+        this._membershipOfferingName = membershipOfferingName;
+    }
+
+    /** Accessor methods */
+
+    get email(): string {
+        return this._email;
+    }
+
+    get contactFirstName(): string {
+        return this._contactFirstName;
+    }
+
+    get contactLastName(): string {
+        return this._contactLastName;
+    }
+
+    get zipCode(): string {
+        return this._zipCode;
+    }
+
+    get memberShipExternalId(): string {
+        return this._membershipExternalId;
+    }
+
+    get membershipPrimaryFirstName(): string {
+        return this._membershipPrimaryFirstName;
+    }
+
+    get membershipPrimaryLastName(): string {
+        return this._membershipPrimaryLastName;
+    }
+
+    get membershipLevelName(): string {
+        return this._membershipLevelName;
+    }
+
+    get membershipOfferingName(): string {
+        return this._membershipOfferingName;
+    }
+
 }
