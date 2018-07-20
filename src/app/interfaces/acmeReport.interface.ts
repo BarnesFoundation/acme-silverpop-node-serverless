da84b008e9d8426074d790ba600e2883cc2ef050
Christopher Jativa <cjativa@barnesfoundation.org>
2018-07-20 16:36:49 -0400

    created interface for AcmeReport

---
+++ src/app/interfaces/acmeReport.interface.ts
@@ -0,0 +1,27 @@
+import { ReportEnums } from '@enums/report.enums';
+
+interface AcmeReport {
+
+    type: ReportEnums;
+    path: string;
+}
+
+const AcmeReportList = [
+
+    {   // Sales Report
+        type: ReportEnums.SALES_REPORT,
+        path: 'b2b/analytics/report/execute/58c1a8c368d6093a3866db70'
+    },
+
+    {   // Transaction Report
+        type: ReportEnums.TRANSACTION_REPORT,
+        path: 'b2b/analytics/report/execute/58c1b3ab1f021613ddf20329'
+    },
+
+    {   // Membership Report
+        type: ReportEnums.MEMBERSHIP_REPORT,
+        path: 'b2b/analytics/report/execute/58c1d056c1a3ef4d470db22e'
+    }
+]
+
+export { AcmeReport, AcmeReportList }
