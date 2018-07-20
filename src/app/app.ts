c0e0f721d3d650d7fb776d28942a951d272c77aa
Christopher Jativa <cjativa@barnesfoundation.org>
2018-07-20 12:13:24 -0400

    added functions for handling the reports received from different endpoints

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,74 @@
-import { Config } from '../utils/config';
-import { Server } from './server';
+import { Config } from '@utils/config';
+import { Reports } from '@interfaces/reports';
+import * as https from 'https';
+import * as request from 'request-promise-native';
 
- 
-const port = Config.port;
+
 const apiRootUrl = Config.apiRootUrl;
+const apiKey = Config.apiKey;
 
-const server = new Server().express;
+function main() {
 
-server.listen(port, (error) => {
+    // Endpoints for the API
+    const salesReportEndpoint = 'b2b/analytics/report/execute/58c1a8c368d6093a3866db70';
+    const transactionReportEndpoint = 'b2b/analytics/report/execute/58c1b3ab1f021613ddf20329';
+    const membershipReportEndpoint = 'b2b/analytics/report/execute/58c1d056c1a3ef4d470db22e';
 
-    if (error) { return console.log(error); } 
 
-    console.log(`Server is listening on ${port}`);
-    console.log(`API root url is ${apiRootUrl}` )
-});
+    const url = apiRootUrl + salesReportEndpoint;
+    connectEndpoint(Reports.SALES_REPORT, url);
+
+
+}
+
+function connectEndpoint(reportType: string, requestUrl: string) {
+
+    const options = {
+        url: requestUrl,
+        headers: {
+            'Accept': 'application/json',
+            'x-acme-api-key': apiKey
+        }, json: true
+    }
+
+    request.get(options)
+        .then((response) => {
+            
+            switch (reportType) {
+
+                case Reports.SALES_REPORT:
+                    processSalesReport(response);
+                    break;
+
+                case Reports.MEMBERSHIP_REPORT:
+                    processMembershipReport(response);
+                    break;
+
+                case Reports.TRANSACTION_REPORT:
+                    processTransactionReport(response);
+                    break;
+            }
+        });
+}
+
+function processSalesReport(report) {
+
+    console.log(report);
+
+    console.log('CheckInStatus', 'ConversionStatus', 'OrderNumber');
+    for (let i = 0; i < 20; i++) {
+        console.log('Row: ', report.resultFieldList[0].values[i], report.resultFieldList[1].values[i], report.resultFieldList[2].values[i]);
+    }
+
+
+}
+
+function processMembershipReport(report) {
+    /** to do */
+}
+
+function processTransactionReport(report) {
+    /** to do */
+}
+
+main(); 
