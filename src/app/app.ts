2f0cdef29cba4488ce3fdfdda8351a91c4286760
Christopher Jativa <cjativa@barnesfoundation.org>
2018-07-23 11:08:54 -0400

    completed processMembershipReport() function

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Config } from '@utils/config';
 import { ReportEnums } from '@enums/report.enums';
 import { AcmeReport, AcmeReportList } from '@interfaces/acmeReport.interface'
-import { TransactionReportFieldIndices as t, SalesReportFieldIndices as s } from '@enums/reportFieldIndices.enum';
+import { TransactionReportFieldIndices as t, SalesReportFieldIndices as s, MembershipReportFieldIndices as m } from '@enums/reportFieldIndices.enum';
 import { SalesReportItem } from '@classes/salesReportItem';
+import { MembershipReportItem } from '@classes/membershipReportItem';
 import { TransactionReportItem } from '@classes/transactionReportItem';
+import { CustomerCard } from '@classes/customerCard';
 import * as request from 'request-promise-native';
+
 
 const apiRootUrl = Config.apiRootUrl;
 const apiKey = Config.apiKey;
@@ -14,18 +17,22 @@ function main() {
     getReports();
 }
 
+/** Iterates fetching through the list of available report types */
 function getReports() {
 
     // For each type of report
-    AcmeReportList.forEach(report => {
+    /* AcmeReportList.forEach(report => {
 
         const url = apiRootUrl + report.path;
 
         // Connect to the endpoint 
         getReportFromEndpoint(report.type, url);
-    });
+    }); */
+
+    getReportFromEndpoint(AcmeReportList[2].type, apiRootUrl + AcmeReportList[2].path);
 }
 
+/** Fetches report from the specified endpoint */
 function getReportFromEndpoint(reportType: string, requestUrl: string) {
 
     // Configure options for the http request
@@ -59,7 +66,7 @@ function getReportFromEndpoint(reportType: string, requestUrl: string) {
         });
 }
 
-/** Processes a Sales Report */
+/** Processes the Sales Report */
 function processSalesReport(report) {
 
     // Empty list of Sales Report Items
@@ -69,7 +76,7 @@ function processSalesReport(report) {
     const results = report.resultFieldList;
     const resultsCount = 20 // report.resultFieldList[0].values.length;
 
-    for (let i = 0; i < resultsCount; i++) {
+    for (let i = 0; i < resultsCount; i ++) {
 
         // Extract fields from the values list
         let checkInStatus = results[s.CheckInStatus].values[i];
@@ -81,6 +88,7 @@ function processSalesReport(report) {
     }
 }
 
+/** Process the Transaction Report */
 function processTransactionReport(report) {
 
     // Empty list of Transaction Report Items
@@ -88,9 +96,9 @@ function processTransactionReport(report) {
 
     // List and count of results in the report
     const results = report.resultFieldList;
-    const resultsCount = 20 // report.resultFieldList[0].values.length;
+    const resultsCount = 20; // report.resultFieldList[0].values.length;
 
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < resultsCount; i ++) {
 
         let accountName = results[t.OrganizationName].values[i];
         let accountCategoryName = results[t.OrganizationCategoryName].values[i];
@@ -123,10 +131,54 @@ function processTransactionReport(report) {
     }
 }
 
+/** Process the Membership Report */
 function processMembershipReport(report) {
-    /** to do */
+    
+    // Empty list of Membership Report items
+    const membershipReportItems: MembershipReportItem[] = [];
 
-    //console.log(report);
+    // List and count of results in the report
+    const results = report.resultFieldList;
+    const resultsCount = 20; // report.resultFieldList[0].values.length;
+    
+    for (let i = 0; i < resultsCount; i ++) {
+
+        let membershipNumber = results[m.MembershipNumber].values[i];
+        let membershipLevelName = results[m.MembershipLevelName].values[i];
+        let membershipOfferingName = results[m.MembershipOfferingName].values[i];
+        let membershipSource = results[m.MembershipSource].values[i];
+        let membershipExternalMembershipId = results[m.MembershipExternalMembershipId].values[i];
+        let membershipJoinDate = results[m.MembershipJoinDate].values[i];
+        let membershipStartDate = results[m.MembershipStartDate].values[i];
+        let membershipExpirationDate = results[m.MembershipExpirationDate].values[i];
+        let membershipDuration = results[m.MembershipDuration].values[i];
+        let membershipStanding = results[m.MembershipStanding].values[i];
+        let membershipIsGifted = results[m.MembershipIsGifted].values[i];
+        let membershipProgramName = results[m.RE_MembershipProgramName].values[i];
+        let membershipCategoryName = results[m.RE_MembershipCategoryName].values[i];
+        let membershipFund = results[m.RE_MembershipFund].values[i];
+        let membershipCampaign = results[m.RE_MembershipCampaign].values[i];
+        let membershipAppeal = results[m.RE_MembershipAppeal].values[i];
+        
+        // Customer Card values
+        let cardType = results[m.CardType].values[i];
+        let cardName = results[m.CardName].values[i];
+        let cardStartDate = results[m.CardStartDate].values[i];
+        let cardExpirationDate = results[m.CardExpirationDate].values[i];
+        let cardCustomerPrimaryCity = results[m.CardCustomerPrimaryCity].values[i];
+        let cardCustomerPrimaryState = results[m.CardCustomerPrimaryState].values[i];
+        let cardCustomerPrimaryZip = results[m.CardCustomerPrimaryZip].values[i];
+        let cardCustomerEmail = results[m.CardCustomerEmail].values[i];
+
+        // Create new Customer Card
+        const customerCard = new CustomerCard(cardType, cardName, cardStartDate, cardExpirationDate, cardCustomerPrimaryCity, cardCustomerPrimaryState, cardCustomerPrimaryZip, cardCustomerEmail);
+        
+        // Create new Membership Report Item and store it
+        membershipReportItems.push(new MembershipReportItem(membershipNumber, membershipLevelName, membershipOfferingName, membershipSource, membershipExternalMembershipId, membershipJoinDate, membershipStartDate,
+            membershipExpirationDate, membershipDuration, membershipStanding, membershipIsGifted, membershipProgramName, membershipCategoryName, membershipFund, membershipCampaign, membershipAppeal, customerCard));
+    }
+
+    console.log(membershipReportItems[0]);
 }
 
 
