5fada8dec589f2a87d6c3b9d5e14cc30b9616eac
Christopher Jativa <cjativa@barnesfoundation.org>
2018-07-24 11:07:51 -0400

    modified handler to execute a node command

--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
 import { main } from './src/app/app';
 
+const exec = require('child_process').exec;
+
 exports.handler = async (event) => {
-    main();
+
+    let command = 'node -r ./tsconfig-paths-bootstrap.js src/app/app.js';
+
+    exec(command, function (error, stdout, stderr) {
+
+        if (stderr || error) {
+            console.log(error);
+        } else {
+            console.log(`stdout: ${stdout}`);
+        }
+    });
 }
+
+exports.handler();
