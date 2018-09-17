39ae99c7466d9dfb664d43935df37d8a03b0c63c
Christopher Jativa <cjativa@barnesfoundation.org>
2018-09-17 15:19:23 -0400

    changed AcmeReportList to be an object

--- src/app/interfaces/acmeReport.interface.ts
+++ src/app/interfaces/acmeReport.interface.ts
@@ -6,22 +6,22 @@ interface AcmeReport {
     path: string;
 }
 
-const AcmeReportList = [
+const AcmeReportList = {
 
-    {   // Sales Report
+    salesReport: {   // Sales Report
         type: ReportEnums.SALES_REPORT,
         path: '58c1a8c368d6093a3866db70/zOrders Test - Source Memberships'
     },
 
-    {   // Transaction Report
+    transactionReport: {   // Transaction Report
         type: ReportEnums.TRANSACTION_REPORT,
         path: '58c1b3ab1f021613ddf20329/zOrdersTest - Source:Transactions'
     },
 
-    {   // Membership Report
+    membershipReport: {   // Membership Report
         type: ReportEnums.MEMBERSHIP_REPORT,
         path: '58c1d056c1a3ef4d470db22e/zOrders Test - Source Memberships'
     }
-]
+}
 
 export { AcmeReport, AcmeReportList }
