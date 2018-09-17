db2d17b31f2912f00be143de6ae23e128b1506da
Christopher Jativa <cjativa@barnesfoundation.org>
2018-09-17 15:07:45 -0400

    modified function name and added link to repository

--- handler.ts
+++ handler.ts
@@ -1,9 +1,14 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
-import { main } from './src/app/app';
+import { Main } from './src/app/app';
 
-export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
+/** This file is the entry point for the AWS Lambda function. When the lambda is executed, the acmeNightlySync function of this file is called.
+ *  The index function calls the Main() function of the actual program. This begins the process of retrieving the CSV reports and placing them in the Watson Campaign Automation SFTP site.
+ *  Repository: https://github.com/BarnesFoundation/business-rules-violation-report
+ */
+
+export const acmeNightlySync: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
   
   console.log('Executing reports');
 
-  main();
+  Main();
 }
