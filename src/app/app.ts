d9916b3ffc82b1c344a43ffb18fde949da9ea76e
Christopher Jativa <cjativa@barnesfoundation.org>
2018-10-03 13:25:20 -0400

    added error logging and handling of single report execution

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,34 +4,53 @@ import * as ssh2SFTPClient from 'ssh2-sftp-client';
 import { Callback } from 'aws-lambda';
 
 import { Config } from '@utils/config';
+import { Input } from '@interfaces/input.interface';
 import { ReportEnums } from '@enums/report.enums';
-import { AcmeReportList } from '@interfaces/acmeReport.interface'
+import { AcmeReportList, AcmeReport } from '@interfaces/acmeReport.interface'
 
-async function main(cb: Callback) {
+async function main(input: Input, cb: Callback) {
 
-    // Get the report objects
-    let transactionReport = AcmeReportList.transactionReport;
-    let membershipReport = AcmeReportList.membershipReport;
-    let salesReport = AcmeReportList.salesReport;
+    // Set the report to be used for this execution
+    let report = setReport(input.reportId)
+    let fileName = report.type + '.csv';
 
-    // Retrieve the report CSVs from ACME
-    let transactionCSV = await getReportFromEndpoint(membershipReport.type, constructReportUrl(membershipReport.path));
-    let membershipCSV = await getReportFromEndpoint(transactionReport.type, constructReportUrl(transactionReport.path));
-    let salesCSV = await getReportFromEndpoint(salesReport.type, constructReportUrl(salesReport.path));
-
+    // Retrieve the CSV for this report from ACME
+    let reportCSV = await getReportFromEndpoint(report.type, constructReportUrl(report.path))
 
     // Upload the reports to the SFTP site
-    try {
-        await uploadToSFTP(transactionCSV, transactionReport.type);
-        await uploadToSFTP(membershipCSV, membershipReport.type);
-        await uploadToSFTP(salesCSV, salesReport.type);
-    }
-    catch (error) {
-        console.log('An error occurred uploading the reports to the SFTP', error);
+    let error;
+    let sftpUploadSuccess = await uploadToSFTP(reportCSV, fileName, error);
+    let result = (sftpUploadSuccess) ? 'Finished uploading ' + fileName + ' to the SFTP site' : 'Failed to upload ' + fileName + ' to the SFTP site';
+
+    // Return success
+    if (sftpUploadSuccess) { cb(null, result); } 
+    
+    // Pass the error to AWS
+    else { cb(error, result) }   
+}
+
+/** Sets the report to be executed and uploaded */
+function setReport(reportId): AcmeReport {
+
+    let report: AcmeReport
+
+    // Match report to be exected
+    switch (reportId) {
+
+        case ReportEnums.SALES_REPORT:
+            report = AcmeReportList.salesReport;
+            break;
+
+        case ReportEnums.TRANSACTION_REPORT:
+            report = AcmeReportList.transactionReport;
+            break;
+
+        case ReportEnums.MEMBERSHIP_REPORT:
+            report = AcmeReportList.membershipReport;
+            break;
     }
 
-
-    cb(null, 'Finished upload all files to SFTP');
+    return report;
 }
 
 /** Iterates fetching through the list of available report types */
@@ -61,7 +80,9 @@ function getReportFromEndpoint(reportType: ReportEnums, requestUrl: string): req
 }
 
 /** Connects to the Watson Campaign Automation SFTP site */
-async function uploadToSFTP(csv, reportType) {
+async function uploadToSFTP(csv, csvName, possibleError) {
+
+    let successfulUpload: boolean;
 
     let sftp = new ssh2SFTPClient();
     let credentials = {
@@ -75,15 +96,30 @@ async function uploadToSFTP(csv, reportType) {
     let Readable = require('stream').Readable;
     let stream = new Readable;
 
-    let fileName = '/upload/' + reportType + '.csv';
+    let sftpPath = '/upload/' + csvName;
 
     // Add the CSV to the stream and newline to signify end of stream
     stream.push(csv);
     stream.push(null);
 
-    await sftp.connect(credentials);
-    await sftp.put(stream, fileName);
-    await sftp.end();
+    try {
+        await sftp.connect(credentials);
+        await sftp.put(stream, sftpPath);
+        await sftp.end();
+
+        // Set that the stfp upload completed
+        successfulUpload = true;
+    }
+    catch (error) {
+        console.log('An error occurred uploading to ' + sftpPath + ' in the SFTP site' , error);
+
+        // Return that an error occurred during the sftp upload
+        possibleError = error;
+        successfulUpload = false;
+    }
+    finally {
+        return successfulUpload;
+    }
 }
 
 export { main as Main };
