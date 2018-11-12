45bd2a2c416854b8998594b18afc17a54a79bc13
Christopher Jativa <cjativa@barnesfoundation.org>
2018-11-12 16:15:23 -0500

    added functions for applying modifications to report data

---
+++ src/app/classes/reportProcessor.ts
@@ -0,0 +1,121 @@
+import * as parse from 'csv-parse';
+import * as stringify from 'csv-stringify';
+import { checkServerIdentity } from 'tls';
+
+/** Parses a provided csv string into an array of objects */
+export function parser(csv): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+
+        // Setup output and the parser
+        let output = [];
+        let parser = parse({ columns: true });
+
+        // Read in the stream for parsing
+        parser.on('readable', () => {
+            let record;
+            while (record = parser.read()) { output.push(record); }
+        });
+
+        // Catch any error
+        parser.on('error', (error) => { console.log(error.message); });
+
+        // Once parsing has finished
+        parser.on('end', () => { resolve(output); });
+
+        // Write the csv to the parser
+        parser.write(csv);
+        parser.end();
+    });
+}
+
+/** Removes guest rows from the provided csv array */
+export function removeGuests(csv: any[]) {
+    return csv.filter((currentElement, index, array) => {
+        return !currentElement.CardName.toLowerCase().includes('Guest of'.toLowerCase()) // Only accept rows where CardName doesn't include 'Guest of'
+    });
+}
+
+export function sortByExpirationDate(csv: any[]) {
+    return csv.sort((a, b) => {
+
+        let aDate = new Date(a.MembershipExpirationDate);
+        let bDate = new Date(b.MembershipExpirationDate);
+
+        if (aDate > bDate) { return -1; }
+
+        if (bDate > aDate) { return 1; }
+
+        if (aDate.getTime() === bDate.getTime()) { return 0; }
+    });
+}
+
+export function createCSV(records: any[]): Promise<string> {
+    return new Promise((resolve, reject) => {
+
+        // Generate the headers for the csv
+        let headers = generateHeaders(records[0]);
+
+        // Create output and stringifier
+        let csvString = [];
+        let stringifier = stringify({ columns: headers, header: true });
+
+        // Read each row into the output
+        stringifier.on('readable', () => {
+            let row;
+            while (row = stringifier.read()) { csvString.push(row); }
+        });
+
+        // Write each record to be stringifier
+        for (let i = 0; i < records.length; i++) {
+
+            let row = [];
+
+            // Build the row by iterating and adding in its properties
+            for (let j = 0; j < headers.length; j++) { row.push(records[i][headers[j].key]); }
+
+            stringifier.write(row);
+        }
+
+        stringifier.end();
+
+        // Catch any errors that occur in reading the input
+        stringifier.on('error', (error) => { reject(error); });
+
+        // Resolve the promise once the stringifier is done
+        stringifier.on('finish', () => { resolve(csvString.join('')); });
+    });
+}
+
+/** Generates a key-value array containing the properties of an object */
+function generateHeaders(record: {}): { key: string }[] {
+
+    let headers = [];
+    let properties = Object.getOwnPropertyNames(record)
+
+    for (let i = 0; i < properties.length; i++) {
+        let header = { key: properties[i] }
+        headers.push(header);
+    }
+    return headers;
+}
+
+export function removeDuplicates(records: any[], key){
+    
+    // Array of encountered records and duplicates filtered out
+    let encountered = [];
+    let filteredSet = [];
+
+    for (let i = 0; i < records.length; i++) {
+
+        let index = records[i][key].toLowerCase();
+
+        // If the record doesn't already exist in the encountered array, add it there and the filtered array
+        if (typeof encountered[index] === 'undefined') {
+
+            // Add that we've encountered the index and add it to filtered set
+            encountered[index] = records[i];
+            filteredSet.push(records[i]);
+        }
+    }
+    return filteredSet;
+}
