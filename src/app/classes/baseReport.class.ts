3b7c08258ec39105833a79549ed022a420433f92
Christopher Jativa <cjativa@barnesfoundation.org>
2018-12-11 11:21:47 -0500

    leaving blank dates as is

--- src/app/classes/baseReport.class.ts
+++ src/app/classes/baseReport.class.ts
@@ -1,9 +1,15 @@
 class BaseReport {
 
     formatDate(isoDate: string): string {
-        let i = new Date(isoDate);
 
-        return (i.getMonth() + 1) + '/' + i.getDate() + '/' + i.getFullYear();
+        // Handle empty date strings
+        if (isoDate.trim().length == 0) { return isoDate; }
+
+        // Return the formatted date
+        else {
+            let i = new Date(isoDate);
+            return (i.getMonth() + 1) + '/' + i.getDate() + '/' + i.getFullYear();
+        }
     }
 }
 
