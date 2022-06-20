820e46bcdeb501553c02ccc1fc826c62b1c5a119
Christopher Jativa <cjativa@barnesfoundation.org>
2022-06-20 17:16:36 -0400

    add back the report ids for our ACME reports we retrieve data from

--- src/app/interfaces/acmeReport_Template.interface.ts
+++ src/app/interfaces/acmeReport_Template.interface.ts
@@ -10,22 +10,22 @@ const AcmeReportList = {
 
     salesReport: {   // Sales Report
         type: ReportEnums.SALES_REPORT,
-        path: ""// ACME id
+        path: "58c1a8c368d6093a3866db70"// ACME id
     },
 
     transactionReport: {   // Transaction Report
         type: ReportEnums.TRANSACTION_REPORT,
-        path: ""// ACME id
+        path: "58c1b3ab1f021613ddf20329"// ACME id
     },
 
     membershipReport: {   // Membership Report
         type: ReportEnums.MEMBERSHIP_REPORT,
-        path: ""// ACME id
+        path: "58c1d056c1a3ef4d470db22e"// ACME id
     },
 
     contactReport: {    // Contact Report
         type: ReportEnums.CONTACT_REPORT,
-        path: ""// ACME id
+        path: "58c1b3ab1f021613ddf20329"// ACME id
     }
 }
 
